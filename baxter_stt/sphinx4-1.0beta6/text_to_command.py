0c7f6ce594957233145529f8832fa383484932f8
dave-abel <dave.abel.cs@gmail.com>
2014-02-04 11:39:17 -0500

    Updated readme and a few small other things in stt/ttc

--- baxter_stt/sphinx4-1.0beta6/text_to_command.py
+++ baxter_stt/sphinx4-1.0beta6/text_to_command.py
@@ -1,9 +1,15 @@
-import re
 import rospy
 from std_msgs.msg import String
 
 def callback(data):
 	rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
+
+	# Do keyword detection here
+	if "block" in data.data:
+		print "Block command detected"
+	elif "can" in data.data:
+		print "Can command detected"
+	# etc..
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
