215e254e6197494437690a4a46527e4a9ec6a838
stephen <brawner@gmail.com>
2014-02-10 18:01:20 -0500

    cleanup

--- baxter_kinect_calibration/build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;image_geometry;image_transport;pcl_ros;roscpp;tf;visualization_msgs;std_msgs;tf2;message_runtime;sensor_msgs;geometry_msgs;resource_retriever;pcl_conversions;dynamic_reconfigure".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "baxter_kinect_calibration"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.2.0"
