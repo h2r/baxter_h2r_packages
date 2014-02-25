9be8f3937d170246b43302d334d8a8f2bfc14c6a
dave-abel <dave.abel.cs@gmail.com>
2014-02-25 10:16:37 -0500

    fixed pathing issue w/ jar and launch file

--- baxter_speech_command/scripts/speech_to_text.py
+++ baxter_speech_command/scripts/speech_to_text.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 import subprocess
 import rospy
+import roslib
 from std_msgs.msg import String
 import os
 
 try:
 	pub = rospy.Publisher('speech_command', String)
 	rospy.init_node('speaker')
+	
+	jar_filename = roslib.packages.get_pkg_dir("baxter_speech_command") + "/src/bin/BaxterSpeech.jar"
 
-	proc = subprocess.Popen(["java","-mx312m", "-jar", "../src/bin/BaxterSpeech.jar"], stdout=subprocess.PIPE)
+	proc = subprocess.Popen(["java","-mx312m", "-jar", jar_filename], stdout=subprocess.PIPE)
+#	proc = subprocess.Popen(["java","-mx312m", "-jar", "../src/bin/BaxterSpeech.jar"], stdout=subprocess.PIPE)
 	print "Start Speaking. Press Crtl-C to quit."
 	while True:
 		line = proc.stdout.readline()
