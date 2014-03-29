0c54750b768cffc351ef1f2aa18120c43babcea7
James MacGlashan <jmacglashan@gmail.com>
2014-03-29 17:03:43 -0400

    pose feature vector code for getting baxter to recognize whether you want to hug, high five, or fist bump.

---
+++ pose_feature_vector/scripts/detect_pose.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+
+import sys
+import rospy
+import tf
+import math
+import numpy as np
+from geometry_msgs.msg import PoseStamped,  Pose, Point, Quaternion, Vector3
+from tf.transformations import quaternion_from_euler
+from pose_feature_vector.msg import PersonFVMsg
+from std_msgs.msg import Header, String
+from baxter_props import BaxterProps
+
+from sklearn import linear_model
+
+
+global logreg
+global classToPoseMap
+global props
+global behaviorOccurring
+global curRecognizedPose
+global numConsecutiveInPose
+
+behaviorOccurring = False
+curRecognizedPose = 3
+numConsecutiveInPose = 0
+
+classToPoseMap = {0: 'hug', 1: 'high five', 2: 'fist bump', 3: 'null pose', 4: 'psi'}
+
+def vec3ToArray(v3):
+	return [v3.x, v3.y, v3.z]
+
+def vecSansDistance(msg):
+	fv = []
+	fv += vec3ToArray(msg.rh)
+	fv += vec3ToArray(msg.re)
+	fv += vec3ToArray(msg.lh)
+	fv += vec3ToArray(msg.le)
+	return fv
+
+
+
+def callback(data):
+
+	global classToPoseMap
+	global logreg
+	global props
+	global behaviorOccurring
+	global curRecognizedPose
+	global numConsecutiveInPose
+
+
+	if not behaviorOccurring:
+
+		behaviorOccurring = True
+
+		fv = vecSansDistance(data)
+
+		classId = int(logreg.predict(fv)[0])
+		className = classToPoseMap[classId]
+
+		if classId != curRecognizedPose:
+			curRecognizedPose = classId
+			numConsecutiveInPose = 0
+
+		elif curRecognizedPose != 3 and curRecognizedPose != 4:
+
+			if numConsecutiveInPose < 15:
+				numConsecutiveInPose += 1
+				print 'num consecutive', className, numConsecutiveInPose
+
+			else:
+
+				print 'performing:', className
+				numConsecutiveInPose = 0
+
+				
+				try:
+
+					if classId == 0:
+						props.hug(0.4, 10.0)
+					elif classId == 1:
+						props.five(Point(x=0.56, y=0.81, z=0.86), "left")
+					elif classId == 2:
+						props.bump(Point(x=0.9, y=0.7, z=0.25), "left")
+
+				except:
+					print 'Could not process request'
+				
+				
+
+		behaviorOccurring = False
+
+	else:
+		print 'skipping'
+
+
+
+def main():
+
+	global logreg
+	global props
+
+	print 'loading classifier'
+	data = np.genfromtxt(sys.argv[1], delimiter=',')
+	X = data[:, 1:]
+	Y = data[:, 0]
+
+	logreg = linear_model.LogisticRegression(C=1e5)
+	logreg.fit(X, Y)
+
+	print 'ready for inputs'
+
+	
+
+	rospy.init_node('pose_detector')
+
+	props = BaxterProps()
+
+	rospy.Subscriber('pose_feature_vector', PersonFVMsg, callback, queue_size=1)
+
+	rospy.spin()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print 'requires single parameter of path to pose training data; exiting'
+		sys.exit()
+	main()
---
+++ pose_feature_vector/scripts/parseTFToFV.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import tf
+import math
+from geometry_msgs.msg import PoseStamped,  Pose, Point, Quaternion, Vector3
+from tf.transformations import quaternion_from_euler
+from pose_feature_vector.msg import PersonFVMsg
+from std_msgs.msg import Header, String
+
+global tfListener
+global frameID
+global seq
+global userID
+
+
+def mag3(vec):
+	return math.sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2])
+
+def norm3(vec):
+	m = mag3(vec)
+	return (vec[0]/m, vec[1]/m, vec[2]/m)
+
+def translate3(point, origin):
+	return (point[0]-origin[0], point[1]-origin[1], point[2]-origin[2])
+
+def getFeatureVector(now):
+
+	global tfListener
+	global userID
+
+	trh = tfListener.lookupTransform(frameID, "right_hand_" + userID, now)
+	tlh = tfListener.lookupTransform(frameID, "left_hand_" + userID, now)
+	tre = tfListener.lookupTransform(frameID, "right_elbow_" + userID, now)
+	tle = tfListener.lookupTransform(frameID, "left_elbow_" + userID, now)
+	thead = tfListener.lookupTransform(frameID, "head_" + userID, now)
+	
+	trrh = norm3(translate3(trh[0], thead[0]))
+	trlh = norm3(translate3(tlh[0], thead[0]))
+	trre = norm3(translate3(tre[0], thead[0]))
+	trle = norm3(translate3(tle[0], thead[0]))
+
+	fv = []
+	fv += trrh
+	fv += trlh
+	fv += trre
+	fv += trle
+	fv.append(thead[0][0]) #head distance
+
+	return fv
+
+
+def setV3(v3, fv, sIndex):
+	v3.x = fv[sIndex]
+	v3.y = fv[sIndex+1]
+	v3.z = fv[sIndex+2]
+
+def fvToMsg(fv, now):
+
+	global seq
+
+	fvMsg = PersonFVMsg()
+	
+	header = Header()
+	header.seq = seq
+	header.stamp = now
+	header.frame_id = frameID
+
+	rhv = Vector3()
+	rev = Vector3()
+	lhv = Vector3()
+	lev = Vector3()
+
+	setV3(rhv, fv, 0)
+	setV3(lhv, fv, 3)
+	setV3(rev, fv, 6)
+	setV3(lev, fv, 9)
+
+	fvMsg.header = header
+	fvMsg.rh = rhv
+	fvMsg.re = rev
+	fvMsg.lh = lhv
+	fvMsg.le = lev
+
+	fvMsg.distance = fv[12]
+
+	return fvMsg
+
+def setMostRecentUser():
+	global tfListener
+	global frameID
+	global userID
+
+	allFramesString = tfListener.getFrameStrings()
+	onlyUsers = set([line for line in allFramesString if 'right_elbow_' in line])
+	n = len('right_elbow_')
+	userIDs = [el[n:] for el in onlyUsers]
+	if len(userIDs) > 0:
+		mostRecentUID = userIDs[0]
+		mostRecentTime = tfListener.getLatestCommonTime(frameID, 'right_elbow_' + mostRecentUID).secs
+		for uid in userIDs:
+			compTime = tfListener.getLatestCommonTime(frameID, 'right_elbow_' + uid).secs
+			if compTime > mostRecentTime:
+				mostRecentUID = uid
+				mostRecentTime = compTime
+		userID = mostRecentUID
+	else:
+		userID = ''
+			
+
+
+
+
+
+def main():
+
+	global seq
+	global tfListener
+	global userID
+	seq = 0
+
+	pub = rospy.Publisher('pose_feature_vector', PersonFVMsg)
+	rospy.init_node('poseFV')
+
+	tfListener = tf.TransformListener()
+
+	r = rospy.Rate(10) #1hz
+
+	publishWait = 40
+
+	#filePath = sys.argv[1]
+
+
+	while not rospy.is_shutdown():
+		now = rospy.Time(0)
+
+		setMostRecentUser()
+		
+		if userID != '':
+			try:
+				tfListener.waitForTransform(frameID, "right_hand_" + userID, now, rospy.Duration(1))
+				if publishWait == 0:
+					fv = getFeatureVector(now)
+					#print fv
+					curMsg = fvToMsg(fv, now)
+					pub.publish(curMsg)
+					seq += 1
+
+				else:
+					print 'publishing in', publishWait
+					publishWait -= 1
+			except tf.Exception:
+				print "waiting for user"
+		else:
+			print 'waiting for user'
+
+		r.sleep()
+		
+
+if __name__ == '__main__':
+	#note that you should set: rosparam set openni_tracker/camera_frame_id "camera_depth_optical_frame"
+	#before tracker code is launched
+	frameID = str(rospy.get_param('/openni_tracker/camera_frame_id'))
+	#frameID = 'openni_depth_frame'
+	print frameID
+	try:
+		main()
+	except rospy.ROSInterruptException: pass
