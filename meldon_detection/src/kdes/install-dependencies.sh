0828e3f5ecebfb765453523b4d4d524067f6fca5
Stephen <brawner@gmail.com>
2014-06-04 13:08:30 -0400

    removing meldon detection

--- meldon_detection/src/kdes/install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get install cmake libopenmpi-dev libhdf5-serial-dev libboost-all-dev libhdf5-7 libhdf5-dev libeigen3-dev
-cd ./dependencies/matio/
-./configure
-make CFLAGS=-DH5_USE_16_API
-sudo make install
-sudo ldconfig
