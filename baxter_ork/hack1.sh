2355210dcf2019dcccf17db84a8c452ca5f44c05
stephen <brawner@gmail.com>
2014-08-14 16:01:24 -0400

    fixing CMakeLists

--- baxter_ork/hack1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosrun object_recognition_core detection -c $(rospack find baxter_ork)/conf/detection.ros.ork
--- baxter_speech_command/src/src/jsapi2/javax.speech/src/javax/speech/EnginePropertyListener.java
+++
@@ -1,7 +0,0 @@
-package javax.speech;
-
-//Comp. 2.0.6
-
-public interface EnginePropertyListener {
-    void propertyUpdate(EnginePropertyEvent event);
-}
