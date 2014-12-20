42918d7f1e8336a91bfd37297b8949b267e94fc5
Stefanie <stefie10@cs.brown.edu>
2014-12-20 16:05:45 -0500

    creating proper api

--- baxter_screen_writer/src/baxter_screen_writer/animator.py
+++ baxter_screen_writer/src/baxter_screen_writer/animator.py
@@ -10,7 +10,7 @@ import cv_bridge
 import rospy
 import rospkg
 from sensor_msgs.msg import Image
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Float32
 
 class Animation:
     def __init__(self, directory):
@@ -19,6 +19,7 @@ class Animation:
         self.images = [cv2.imread(path) for path in self.fnames]
         self.current_value = 0
         self.current_idx = 0
+        self.set_velocity(1/40.0)
         self.target = None
 
         self.image_publisher = rospy.Publisher("/robot/xdisplay", Image,
@@ -26,26 +27,48 @@ class Animation:
         self.value_subscriber = rospy.Subscriber("/confusion/value", Int32, self.set_value)
 
         self.target_subscriber = rospy.Subscriber("/confusion/target", Int32, self.set_target)
+        self.velocity_subscriber = rospy.Subscriber("/confusion/velocity", Int32, self.set_velocity)
 
-        self.animation_timer = rospy.Timer(rospy.Duration(1.0/5.0), self.animate)
 
+    def publish_state(self):
+        self.state_publisher.publish(msg)
  
-
+    def set_velocity(self, velocity):
+        if isinstance(velocity, Float32):
+            velocity = velocity.data
+        self.velocity = velocity
+        self.animation_timer = rospy.Timer(rospy.Duration(self.velocity), self.animate)      
     def set_idx(self, idx):
         self.current_idx = idx;
         self.current_value = int((float(idx) / len(self.images)) * 100)
+        self.checkrep()
+        return self.publish
+
+
+    def set_value(self, value):
+        if isinstance(value, Int32):
+            value = value.data
+        self.current_value = value
+        self.current_idx = int((value / 100.0) * len(self.images))
+        self.checkrep()
+        return self.publish()
+
+    def checkrep(self):
+        assert 0 <= self.current_idx < len(self.images), self.current_idx
+        assert 0 <= self.current_value < 100, self.current_value
+        assert self.target == None or (0 <= self.target < 100), self.target
+    def publish(self):
         msg = cv_bridge.CvBridge().cv2_to_imgmsg(self.image, encoding="bgr8")
         self.image_publisher.publish(msg)
         return self.images[self.current_idx]
         
-    def set_value(self, value):
-        if isinstance(value, Int32):
-            value = value.data
-        idx = int((value / 100.0) * len(self.images))
-        return self.set_idx(idx)
+
 
     def set_target(self, target):
-        print "setting target"
+        if isinstance(target, Int32):
+            target = target.data
+
+        print "setting target", target
         self.target = target
 
 
@@ -55,10 +78,11 @@ class Animation:
         
     def animate(self, time):
         if self.target != None:
+            print "target", self.target, self.current_value
             if self.target < self.current_value:
-                self.set_value(self.current_value + 1)
-            elif self.target > self.current_value:
                 self.set_value(self.current_value - 1)
+            elif self.target > self.current_value:
+                self.set_value(self.current_value + 1)
             elif self.target == self.current_value:
                 self.target = None
             else:
@@ -81,18 +105,29 @@ class MainWindow(QMainWindow, animator_ui.Ui_MainWindow):
         self.animation = Animation(path)
         self.value_publisher = rospy.Publisher("/confusion/value", Int32,
                                                queue_size=10)
-        self.value_subscriber = rospy.Subscriber("/confusion/value", Int32,
-                                                 self.displayConfusionValue)
+
         self.target_publisher = rospy.Publisher("/confusion/target", Int32,
                                                 queue_size=10)
+
+        self.value_subscriber = rospy.Subscriber("/confusion/value", Int32,
+                                                 self.displayConfusionValue)
+        self.target_subscriber = rospy.Subscriber("/confusion/target", Int32,
+                                                  self.displayConfusionTarget)
+        self.velocity_subscriber = rospy.Subscriber("/confusion/target", Int32,
+                                                    self.displayConfusionVelocity)
 
     def displayConfusionValue(self, value):
         value = value.data
         self.confusionValueLabel.setText(str(value))
 
-        self.currentValueSlider.setValue(int(value))
+    def displayConfusionTarget(self, target):
+        target = target.data
+        self.confusionValueLabel.setText(str(target))
 
-        
+    def displayConfusionVelocity(self, velocity):
+        velocity = velocity.data
+        self.confusionVelocityLabel.setText(str(velocity))
+
     def confusionTargetChanged(self, value):
         self.target_publisher.publish(value)
 
