9ed19a35121182f27fa5e3b1bde7822f3824a223
Stefanie Tellex <stefie10@cs.brown.edu>
2015-01-02 18:19:40 -0500

    both cameras

--- camera_controller/src/camera_controller/camera_controller.py
+++ camera_controller/src/camera_controller/camera_controller.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 
-import time
-
-
 from baxter_interface import CameraController
 
 def main():
-    print "camera"
-
     # exposure = 80, gain=50 looks good on black, terrible on beige    
-
-    left_hand_camera = CameraController('left_hand_camera')
-
-    left_hand_camera.resolution = (1280,800)
-    left_hand_camera.exposure = 50
-    #left_hand_camera.exposure = CameraController.CONTROL_AUTO
-    left_hand_camera.gain = 30    #left_hand_camera.gain = CameraController.CONTROL_AUTO
-    left_hand_camera.white_balance_red = CameraController.CONTROL_AUTO
-    left_hand_camera.white_balance_green = CameraController.CONTROL_AUTO
-    left_hand_camera.white_balance_blue = CameraController.CONTROL_AUTO
-    left_hand_camera.open()
+    for camera_name in ('left_hand_camera', 'right_hand_camera'):
+        print "setting", camera_name
+        camera = CameraController(camera_name)
+        camera.resolution = (1280,800)
+        camera.exposure = 50
+        camera.gain = 30    #left_hand_camera.gain = CameraController.CONTROL_AUTO
+        camera.white_balance_red = CameraController.CONTROL_AUTO
+        camera.white_balance_green = CameraController.CONTROL_AUTO
+        camera.white_balance_blue = CameraController.CONTROL_AUTO
+        camera.open()
 
 
 if __name__ == "__main__":
